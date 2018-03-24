e4616ad16ca3af2899cef1caf6c0bb19289fab8e
沈钱 <qian.shen@utsoft.cn>
2018-03-24 16:50:35 -0700

    初始化工程

---
+++ SQKit/SQKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SQKit
+//
+//  Created by Seedy on 2018/3/24.
+//  Copyright © 2018年 Seedy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SQKit/SQKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SQKit
+//
+//  Created by Seedy on 2018/3/24.
+//  Copyright © 2018年 Seedy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
